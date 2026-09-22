4483398c112d6195aa91f549278e42796ecad85a
Juan Ojeda-0192841 <juanojeda0219@gmail.com>
2026-09-22 08:01:28 -0600

    Entrega parcial

---
+++ src/Ejercicio2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int filas = 4;    // 4 Sucursales
+        int columnas = 5; // 5 Productos
+
+        int[][] ventas = new int[filas][columnas];
+        int[] totalPorSucursal = new int[filas];
+        int[] totalPorProducto = new int[columnas];
+        int registrosMayores30 = 0;
+
+        
+    }
+}
